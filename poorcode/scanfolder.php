209a185c3476ae525dceb332bdde5e452698b68d
VoskanBaldryan <baldryanvoskan98@gmail.com>
2021-07-20 19:17:15 +0400

    folder fix

--- poorcode/scanfolder.php
+++ poorcode/scanfolder.php
@@ -1,27 +1,34 @@
 <?php
-        $folder_path = './folders';
-        if (isset($_GET['page'])) {
-            $folder_path = './folders/' . $_GET['page'];
-        }
-        if(is_array(scandir($folder_path)) && count(scandir($folder_path)) > 2){
-            foreach (scandir($folder_path) as $folder) {
-                if ($folder != '.' && $folder != '..'){
-                    if(is_dir($folder_path . '/'. $folder)){
-                        $arr[] = [
-                            'name' => $folder,
-                            'size' => filesize($folder_path . '/' . $folder),
-                            'date' => filectime($folder_path . '/' . $folder), //date ("Y-m-d H:i:s", filectime('./folders/' .$folder)),
-                            'type' => 'folder'
-                        ];
-                    }else{
-                        $path_parts = pathinfo($folder_path . '/' . $folder,  PATHINFO_EXTENSION);
-                        $arr[] = [
-                            'name' => $folder,
-                            'size' => filesize($folder_path . '/' . $folder),
-                            'date' => filectime($folder_path . '/' . $folder), //date ("Y-m-d H:i:s", filectime('./folders/' .$folder)),
-                            'type' => $path_parts
-                        ];
-                    }
-                }
+
+
+$folder_path = './folders';
+if (isset($_GET['page'])) {
+    $folder_path = './folders/' . $_GET['page'];
+}
+
+if (!file_exists($folder_path)) {
+    mkdir($folder_path, 0777, true);
+}
+
+if (is_array(scandir($folder_path)) && count(scandir($folder_path)) > 2) {
+    foreach (scandir($folder_path) as $folder) {
+        if ($folder != '.' && $folder != '..') {
+            if (is_dir($folder_path . '/' . $folder)) {
+                $arr[] = [
+                    'name' => $folder,
+                    'size' => filesize($folder_path . '/' . $folder),
+                    'date' => filectime($folder_path . '/' . $folder), //date ("Y-m-d H:i:s", filectime('./folders/' .$folder)),
+                    'type' => 'folder'
+                ];
+            } else {
+                $path_parts = pathinfo($folder_path . '/' . $folder, PATHINFO_EXTENSION);
+                $arr[] = [
+                    'name' => $folder,
+                    'size' => filesize($folder_path . '/' . $folder),
+                    'date' => filectime($folder_path . '/' . $folder), //date ("Y-m-d H:i:s", filectime('./folders/' .$folder)),
+                    'type' => $path_parts
+                ];
             }
         }
+    }
+}
